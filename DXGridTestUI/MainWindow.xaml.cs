2b8e51deaeeccebc7f66f109e021daf1ab610617
Hanh <hanh425@gmail.com>
2015-06-14 18:53:12 +0800

    Sample MVVM project / Devexpress

---
+++ DXGridTestUI/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Mvvm.UI;
+using DevExpress.Data;
+using ViewModel;
+
+namespace DXGridTestUI
+{
+    public class CustomSummaryEventArgsConverter : EventArgsConverterBase<CustomSummaryEventArgs>
+    {
+        public CustomSummaryEventArgsConverter()
+        {
+        }
+
+        protected override object Convert(CustomSummaryEventArgs e)
+        {
+            switch (e.SummaryProcess) {
+                case CustomSummaryProcess.Start: return CustomSummaryArgs.Start;
+                case CustomSummaryProcess.Calculate: 
+                    var n = (int?) e.FieldValue;
+                    return CustomSummaryArgs.NewCalculate(n.Value);
+                case CustomSummaryProcess.Finalize:
+                    Action<int> a = delegate(int sum)
+                    {
+                        e.TotalValue = sum;
+                    };
+                    return CustomSummaryArgs.NewFinish(a);
+                default: return CustomSummaryArgs.Start;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private int n;
+
+        private void GridControl_CustomSummary(object sender, DevExpress.Data.CustomSummaryEventArgs e)
+        {
+            if (e.IsTotalSummary)
+            {
+                Console.Out.WriteLine(e.ToString());
+                switch (e.SummaryProcess)
+                {
+                    case DevExpress.Data.CustomSummaryProcess.Start:
+                        n = 0;
+                        break;
+
+                    case DevExpress.Data.CustomSummaryProcess.Calculate:
+                        var fv = (int?) e.FieldValue;
+                        n = n + fv.Value;
+                        break;
+
+                    case DevExpress.Data.CustomSummaryProcess.Finalize:
+                        e.TotalValue = n;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ DXGridTestVM/ViewModel.fs
@@ -0,0 +1,37 @@
+﻿namespace ViewModel
+
+open DevExpress.Xpf.Mvvm
+open System.Collections.ObjectModel
+
+type Row(n: int) = 
+    inherit BindableBase()
+
+    member x.N with get() = n
+    member x.SqrN with get() = n*n
+    member x.IsEven with get() = n%2 = 0
+
+type CustomSummaryArgs =
+    | Start
+    | Calculate of int
+    | Finish of System.Action<int>
+
+type Data() = 
+    inherit ViewModelBase()
+
+    let mutable s = 0
+    let listOfNumbers = new ObservableCollection<Row>()
+    let customSummary (e: CustomSummaryArgs) = 
+        printfn "%A" e
+        match e with
+        | Start -> s <- 0
+        | Calculate n -> s <- s+n
+        | Finish f -> f.Invoke(s)
+        | _ -> ignore()
+
+    do
+        for i in 1..10 do
+            listOfNumbers.Add(new Row(i))
+
+    member x.Numbers with get() = listOfNumbers
+    member x.CustomSummary with get() = new DelegateCommand<CustomSummaryArgs>(fun e -> customSummary e)
+
